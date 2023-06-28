0b71dfb18d6f55fee56333dcd42ed1c7fbefceeb
JoaoVitttt <102669094+JoaoVitttt@users.noreply.github.com>
2023-06-27 23:38:23 -0300

    Delete testff directory

--- testff/connection/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('PASS', '');
-    define('BASE', 'gerfin');//Nome do Database no MySQL
-
-    $conn = new mysqli(HOST,USER,PASS,BASE);
-
-?>
